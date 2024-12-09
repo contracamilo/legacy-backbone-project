6a4e055536025f552337041163be66ba5fdfbc5f
camiviceroy <camilo.riveradev@gmail.com>
2024-12-09 15:39:53 +0100

    add events, a11y practices and some views

--- js/todoItemsView.js
+++ js/todoItemsView.js
@@ -9,11 +9,41 @@ var TodoItemsView = Backbone.View.extend({
         }
 
         this.model.on('add', this.onAddTodoItem, this);
-        this.model.on('remove', this.onRemoveTodoItem, this);
     },
+
+    onAddTodoItem: function (todoItem) {
+        var view = new TodoItemView({ model: todoItem });
+        this.$el.append(view.render().$el);
+    },
+
+    events: {
+        'click #add': 'onClickAdd',
+        'keypress #newTodoItem': 'onKeyPress'
+    },
+    
+
+    onClickAdd: function () {
+        var $textBox = this.$('#newTodoItem');
+
+        if($textBox.val()){
+            var todoItem = new TodoItem({ description: $textBox.val() });
+            this.model.add(todoItem);
+            $textBox.val('');
+        }
+    },
+
+    onKeyPress: function (e) {
+        if (e.keyCode == 13) {
+            this.onClickAdd();
+        }
+    },
+    
 
     render: function () {
         var self = this;
+
+        this.$el.append('<input type="text" autofocus id="newTodoItem" </input>');
+        this.$el.append('<button id="add">Add</button>');
 
         this.model.each(function (todoItem) {
             var view = new TodoItemView({model: todoItem});
